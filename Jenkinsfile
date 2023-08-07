4f033fbab164ef5bb819b38425ba54b9948f95db
dan-sugrue <95246900+dan-sugrue@users.noreply.github.com>
2023-08-07 09:23:07 -0600

    Add files via upload

---
+++ Jenkinsfile
@@ -0,0 +1,104 @@
+pipeline {
+  agent any
+environment {
+
+SITEBASEURL = null
+APIKEY = null
+PACKAGEFILENAME = null
+initiateInspectionJson = null
+deploymentResponseJson = null
+warnings = null
+errors = null
+DEPLOYMENTNAME = null
+DEPLOYMENTDESCRIPTION = null
+}
+  stages {
+    
+    stage("Install AVM and FitNesse for Appian") {
+      steps {
+        script {
+          // Retrieve and setup AVM
+          
+          bat "if exist adm rmdir /Q /S adm"
+          bat "if exist f4a rmdir /Q /S f4a"
+          bat "mkdir adm\\appian-version-client"
+          bat "mkdir f4a"
+          //jenkinsUtils.shNoTrace("curl -H X-JFrog-Art-Api:$ARTIFACTORYAPIKEY -O $ARTIFACTORYURL/appian-devops/adm.zip")
+          bat "tar -xf devops/adm.zip -C adm"
+          bat "tar -xf adm/appian-adm-versioning-client-2.5.17.zip -C adm/appian-version-client" 
+          def v = unzip zipFile: "adm/appian-adm-versioning-client-2.5.17.zip", dir: "adm/appian-version-client"
+          // Retrieve and setup F4A
+          //jenkinsUtils.shNoTrace("curl -H X-JFrog-Art-Api:$ARTIFACTORYAPIKEY -O $ARTIFACTORYURL/appian-devops/f4a.zip")
+          //bat "tar -xf f4a.zip -C f4a"
+          //sh "cp -a devops/f4a/test_suites/. f4a/FitNesseForAppian/FitNesseRoot/FitNesseForAppian/Examples/"
+          //sh "cp devops/f4a/users.properties f4a/FitNesseForAppian/configs/users.properties"
+
+          
+        }
+      }
+    }
+    stage("Build Package") {
+      steps {
+        script {
+          def properties = readProperties file: "devops\\deploymentmanagement.test.properties"
+          DEPLOYMENTDESCRIPTION = properties['deploymentDescription']
+          DEPLOYMENTNAME = properties['deploymentName']
+          SITEBASEURL = properties['url']
+          APIKEY = properties['siteApiKey']
+          PACKAGEFILENAME = properties['packageFileName']
+          def jenkinsUtils = load "groovy/JenkinsUtils.groovy"
+          jenkinsUtils.buildPackage("version-manager.properties")
+        }
+      }
+    }
+
+    stage("Inspect Package") {
+      steps {
+        script {
+          def properties = readProperties file: "devops\\deploymentmanagement.test.properties"
+          DEPLOYMENTDESCRIPTION = properties['deploymentDescription']
+          DEPLOYMENTNAME = properties['deploymentName']
+          SITEBASEURL = properties['url']
+          APIKEY = properties['siteApiKey']
+          PACKAGEFILENAME = properties['packageFileName']
+          def jenkinsUtils = load "groovy/JenkinsUtils.groovy"
+          jenkinsUtils.inspectPackage("${APPLICATIONNAME}.test.properties")
+        }
+      }
+    }
+    
+    stage("Create Deployment Request") {
+      steps {
+        script {
+          def properties = readProperties file: "devops\\deploymentmanagement.test.properties"
+          DEPLOYMENTDESCRIPTION = properties['deploymentDescription']
+          DEPLOYMENTNAME = properties['deploymentName']
+          SITEBASEURL = properties['url']
+          APIKEY = properties['siteApiKey']
+          PACKAGEFILENAME = properties['packageFileName']
+          def jenkinsUtils = load "groovy/JenkinsUtils.groovy"
+          jenkinsUtils.createDeployment("${APPLICATIONNAME}.test.properties")
+          
+
+
+        }
+      }
+    }
+    stage("Check Deployment Status") {
+      steps {
+        script {
+          def properties = readProperties file: "devops\\deploymentmanagement.test.properties"
+          DEPLOYMENTDESCRIPTION = properties['deploymentDescription']
+          DEPLOYMENTNAME = properties['deploymentName']
+          SITEBASEURL = properties['url']
+          APIKEY = properties['siteApiKey']
+          PACKAGEFILENAME = properties['packageFileName']
+          def jenkinsUtils = load "groovy/JenkinsUtils.groovy"
+          jenkinsUtils.checkDeploymentStatus()
+        }
+      }
+    }
+  }
+}
+
+
---
+++ groovy/JenkinsUtils.groovy
@@ -0,0 +1,131 @@
+#!/usr/bin/env groovy
+
+void runTestsVNC(propertyFile) {
+  sh "cp devops/f4a/" + propertyFile + " f4a/FitNesseForAppian/fitnesse-automation.properties"
+  dir("f4a/FitNesseForAppian") {
+    wrap([$class:'Xvnc', useXauthority: true]) {
+      sh script: "bash ./runFitNesseTest.sh"
+    }
+  }
+}
+
+void runTestsDocker(propertyFile) {
+  sh "cp devops/f4a/" + propertyFile + " f4a/FitNesseForAppian/fitnesse-automation.properties"
+  sh "docker-compose -f docker/docker-compose.yml up &"
+  timeout(2) { //timeout is in minutes
+    waitUntil {
+      def numExpectedContainers = "2"
+      def runningContainers = sh script: "docker ps --format {{.Names}} | grep \"fitnesse-\\(chrome\\|firefox\\)\" | wc -l", returnStdout: true
+      runningContainers = runningContainers.trim()
+      return (runningContainers == numExpectedContainers)
+    }
+  }
+  sleep(10)
+  dir("f4a/FitNesseForAppian") {
+    sh script: "bash ./runFitNesseTest.sh"
+  }
+}
+
+void retrieveLogs(propertyFile) {
+  def test = sh script: "cat \"devops/f4a/${propertyFile}\" | grep \"testPath=\" | cut -d'=' -f2", returnStdout: true
+  test = test.trim().minus(~"\\?.*")
+  def zipName = "${test}_Results.zip"
+  dir("f4a/FitNesseForAppian/FitNesseRoot/files/testResults") {
+    sh "zip -r ${zipName} ${test}/**"
+  }
+  return "f4a/FitNesseForAppian/FitNesseRoot/files/testResults/${zipName}"
+}
+
+void buildPackage(versionPropertyFile) {
+  def status = bat(script:"robocopy devops/adm adm/appian-version-client " + versionPropertyFile,returnStatus:true)
+  println "ROBOCOPY returned ${status}"
+  dir("adm/appian-version-client") {
+    //setProperty("version-manager.properties", "vcUsername", "${REPOUSERNAME}")
+    //setProperty("version-manager.properties", "vcPassword", "${REPOPASSWORD}")
+    //setProperty("version-manager.properties", "appianObjectsRepoPath", "appian/applications/${APPLICATIONNAME}")
+    bat(script:"version-application.bat -vc_username \"$REPOUSERNAME\" -vc_password \"$REPOPASSWORD\" -package_path ../app-package.zip -local_repo_path ./local-repo -appian_objects_repo_path appian/applications/${APPLICATIONNAME}" )
+    bat "tar -xjf ../app-package.zip \"*.zip\""
+    bat "rename \"application_*.zip\" \"deploy-package.zip\""
+    bat "if exist newBundle rmdir /Q /S newBundle"
+    bat "mkdir newBundle"
+    def y = unzip zipFile: "deploy-package.zip", dir: "newBundle"
+    bat "if exist \"newBundle/appian\" rmdir /Q /S \"newBundle/appian\""
+    def z = zip zipFile: "../finalPackage.zip", dir: "newBundle"
+  }
+}
+void inspectPackage(customProperties) {
+  inspectionUrl = SITEBASEURL +"/deployment-management/v1/inspections"
+  String response = null
+  println customProperties
+  if (fileExists("appian/properties/${APPLICATIONNAME}/" + customProperties)) {
+  	println "Properties Exist"
+	response=bat( script:"curl --location  --request POST \"$inspectionUrl\" --header \"Appian-API-Key: $APIKEY\" --form \"zipFile=@\"./adm/finalPackage.zip\"\" --form \"ICF=@\"./appian/properties/${APPLICATIONNAME}/${customProperties}\"\" --form \"json={\"packageFileName\":\"finalPackage.zip\",\"customizationFileName\":\"$customProperties\"}\"", returnStdout: true).trim()
+  } else{
+  	response=bat( script:"curl --location  --request POST \"$inspectionUrl\" --header \"Appian-API-Key: $APIKEY\" --form \"zipFile=@\"./adm/finalPackage.zip\"\" --form \"json={\"packageFileName\":\"finalPackage.zip\"}\"", returnStdout: true).trim()
+  }
+  newResponse = response.readLines().drop(1).join(" ")
+  initiateInspectionJson = new groovy.json.JsonSlurperClassic().parseText(newResponse)
+  println "Inspection Started"
+  println initiateInspectionJson
+  sleep 5
+  String newUrl = SITEBASEURL + "/deployment-management/v1/inspections" + "/" + initiateInspectionJson.uuid +"/"
+  String inspectionResponse = bat(script: "curl --silent --location --request GET \"$newUrl\" --header \"Appian-API-Key: $APIKEY\"" , returnStdout: true).trim()
+  inspectionResponse = inspectionResponse.readLines().drop(1).join(" ")
+  inspectionResponseJson = new groovy.json.JsonSlurperClassic().parseText(inspectionResponse)
+  inspectionStatus = inspectionResponseJson.status
+  while(inspectionStatus.equals("IN_PROGRESS")) {
+    sleep 30
+    inspectionResponse = bat(script: "curl --silent --location --request GET \"$newUrl\" --header \"Appian-API-Key: $APIKEY\"" , returnStdout: true).trim()
+    inspectionResponse = inspectionResponse.readLines().drop(1).join(" ")
+    inspectionResponseJson = new groovy.json.JsonSlurperClassic().parseText(inspectionResponse)
+    inspectionStatus = inspectionResponseJson.status
+  }
+  println inspectionResponseJson
+  warnings = inspectionResponseJson.summary.problems.totalWarnings
+  errors = inspectionResponseJson.summary.problems.totalErrors
+  if(warnings.equals(0) && errors.equals(0)) {
+    println "Inspection Success"
+  } else{
+    error "Inspection Failed, Pipeline Stopped"
+  }
+        
+}
+
+void createDeployment(customProperties) {
+  deploymentUrl = SITEBASEURL + "/deployment-management/v1/deployments"
+  if (fileExists("appian/properties/${APPLICATIONNAME}/" + customProperties)) {
+	String response=bat( script:"curl --silent --location  --request POST \"$deploymentUrl\" --header \"Appian-API-Key: $APIKEY\" --form \"zipFile=@\"./adm/finalPackage.zip\"\" --form \"ICF=@\"./appian/properties/${APPLICATIONNAME}/${customProperties}\"\" --form \"json={\"packageFileName\":\"finalPackage.zip\",\"name\":\"$DEPLOYMENTNAME\",\"customizationFileName\":\"$customProperties\"}\"", returnStdout: true).trim()
+  }
+  String response=bat( script:"curl --silent --location  --request POST \"$deploymentUrl\" --header \"Appian-API-Key: $APIKEY\" --form \"zipFile=@\"./adm/finalPackage.zip\"\" --form \"json={\"packageFileName\":\"finalPackage.zip\",\"name\":\"$DEPLOYMENTNAME\"}\"", returnStdout: true).trim()
+  deploymentResponse = response.readLines().drop(1).join(" ")
+  deploymentResponseJson = new groovy.json.JsonSlurperClassic().parseText(deploymentResponse)
+  println "Deployment Requested"
+}
+
+void checkDeploymentStatus() {
+  sleep 15
+  String newUrl = SITEBASEURL + "/deployment-management/v1/deployments" + "/" + deploymentResponseJson.uuid +"/"
+  String deploymentStatus = bat(script: "curl --silent --location --request GET \"$newUrl\" --header \"Appian-API-Key: $APIKEY\"" , returnStdout: true).trim()
+  deploymentStatus = deploymentStatus.readLines().drop(1).join(" ")
+  deploymentStatusJson = new groovy.json.JsonSlurperClassic().parseText(deploymentStatus)
+  statusVar = deploymentStatusJson.status
+  while (statusVar.equals("IN_PROGRESS")) {
+    sleep 30
+    deploymentStatus = bat(script: "curl --silent --location --request GET \"$newUrl\" --header \"Appian-API-Key: $APIKEY\"" , returnStdout: true).trim()
+    deploymentStatus = deploymentStatus.readLines().drop(1).join(" ")
+    deploymentStatusJson = new groovy.json.JsonSlurperClassic().parseText(deploymentStatus)
+    statusVar = deploymentStatusJson.status
+  }
+  println "Deployment Finished and Status is " + statusVar
+
+}
+
+void setProperty(filePath, property, propertyValue) {
+  shNoTrace("sed -i -e 's|.\\?${property}=.*|${property}=${propertyValue}|' ${filePath}")
+}
+
+def shNoTrace(cmd) {
+  bat '#!/bin/sh -e\n' + cmd
+}
+
+return this
